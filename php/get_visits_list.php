b1b96f853ef7ec3fe4d1723b95d3f4aa22d8743f
Brandon Rene <rene.reza_garcia@hotmail.com>
2022-06-19 15:07:54 -0500

    36%

--- php/get_visits_list.php
+++ php/get_visits_list.php
@@ -5,7 +5,7 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }else{
-    $sql = "SELECT ID_VISIT, NAMEC, SEXO, PHONE, EMAIL, FECHA_VISIT, FECHA_FIN, PAGO, CONCAT('$', TOTAL) AS MONTO FROM VISITS";
+    $sql = "SELECT ID_VISIT, NAMEC, SEXO, PHONE, EMAIL, FECHA_VISIT, FECHA_FIN, PAGO, CONCAT('$', TOTAL) AS MONTO FROM visits";
     $result = mysqli_query($conn, $sql);
 
     if (mysqli_num_rows($result) > 0) {
